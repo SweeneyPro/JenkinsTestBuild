6ffe0c8adfe17500e646d24dd42da9c967e20e70
Sweeney <sweeney.pro@hotmail.co.uk>
2018-11-08 08:59:03 +0000

    Switched to new workspace for Jenkins

--- JenkinsTestBuild/JenkinsTestBuild/CompileBatch.bat
+++ JenkinsTestBuild/JenkinsTestBuild/CompileBatch.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat 
 set CL=/DBOOTTEST
-msbuild C:\Jenkins\workspace\JenkinsTestBuild_master-HSFE4JB3YDTDRHV5TTSFLB7QDJY25TPJ74C6A5YHH6YY2QGFGWPQ\JenkinsTestBuild\ /t:rebuild
+msbuild C:\Jenkins\workspace\TestPipeline\JenkinsTestBuild\ /t:rebuild
