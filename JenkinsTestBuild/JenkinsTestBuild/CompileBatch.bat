990977f77ee8d732827cdea79b9cf54164d274c8
SweeneyPro <sweeney.pro@hotmail.co.uk>
2018-11-13 14:00:31 +0000

    Stripping out Hello World

--- JenkinsTestBuild/JenkinsTestBuild/CompileBatch.bat
+++
@@ -1,3 +0,0 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsx86_amd64.bat 
-set CL=/DBOOTTEST
-msbuild C:\Jenkins\workspace\TestPipeline\JenkinsTestBuild\ /t:rebuild
